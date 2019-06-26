97c0e894c19ad48c17eb8abedb6449721219d5b9
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-06-26 09:42:30 -0700

    following along with Ryan, created function to create files but now my jest isn't working?

--- README.md
+++ README.md
@@ -11,3 +11,18 @@ What steps I need to take:
 after creating files, rename files
   callbacks? still unsure but probably need it
   renameFunction 
+
+## Goals
+Rename all files in a specified based on the content of the file, number in the file name, and the date that the file was last modified.
+
+## Steps
+Recreate the problem
+  -get random animal species
+  -write a bunch of files with animal species
+  
+Get all the files in the target directory
+Rename a file
+Get the last modified date of a file
+Get a files content
+Rename all files with content-oldFileNumber-date
+Clean Up
--- index-test.js
+++ index-test.js
@@ -1,3 +1,20 @@
 const fs = require('fs');
-const createFile = require('./index');
+const { createFiles } = require('./index');
 
+describe ('create files', () => {
+  it('can get a random animal', () => { 
+    const animal = createFiles();
+    expect(animal).toEqual(expect.any(String));
+});
+
+  it('can write a bunch of files with anaimals in them', done => {
+    createFiles('./new-files', 10, err => {
+      expect(err).toBeFalsy();
+
+      fs.readdir('./new-files', { encoding: 'utf8' }, (err, files) => {
+        expect(files).toHaveLength(10);
+        done();
+      });
+    });
+  });
+});
